d1021d0df641256fe59ac83d5ef9443d86500bfc
jacoacoacob <jacoacoacob@protonmail.com>
2023-04-10 12:15:36 -0500

    form field component functions

--- src/models/character_data.rs
+++ src/models/character_data.rs
@@ -5,15 +5,15 @@ use serde::{Deserialize, Serialize};
 use super::character_delta::CharacterDelta;
 
 #[derive(Default, Serialize, Deserialize, Clone, PartialEq, Eq)]
-pub struct Skill {
+pub struct Proficiency {
     pub proficient: bool,
-    pub modifier: usize,
+    pub modifier: String,
 }
 
 #[derive(Serialize, Deserialize, Clone, PartialEq, Eq)]
 pub enum FieldValue {
     String(String),
-    Skill(Skill),
+    Proficiency(Proficiency),
 }
 
 #[derive(Default, Serialize, Deserialize)]
@@ -34,31 +34,31 @@ pub struct CharacterData {
     pub score_charisma: Option<String>,
     pub inspiration: Option<String>,
     pub proficiency_bonus: Option<String>,
-    pub saving_throw_str: Option<String>,
-    pub saving_throw_dexterity: Option<String>,
-    pub saving_throw_constitution: Option<String>,
-    pub saving_throw_intelligence: Option<String>,
-    pub saving_throw_wisdom: Option<String>,
-    pub saving_throw_charisma: Option<String>,
+    pub saving_throw_strength: Option<Proficiency>,
+    pub saving_throw_dexterity: Option<Proficiency>,
+    pub saving_throw_constitution: Option<Proficiency>,
+    pub saving_throw_intelligence: Option<Proficiency>,
+    pub saving_throw_wisdom: Option<Proficiency>,
+    pub saving_throw_charisma: Option<Proficiency>,
     pub death_save_fail: Option<String>,
     pub death_save_success: Option<String>,
-    pub skill_acrobatics: Option<Skill>,
-    pub skill_animal_handling: Option<Skill>,
-    pub skill_athletics: Option<Skill>,
-    pub skill_deception: Option<Skill>,
-    pub skill_history: Option<Skill>,
-    pub skill_insight: Option<Skill>,
-    pub skill_inimidation: Option<Skill>,
-    pub skill_investigation: Option<Skill>,
-    pub skill_medicine: Option<Skill>,
-    pub skill_nature: Option<Skill>,
-    pub skill_perception: Option<Skill>,
-    pub skill_performance: Option<Skill>,
-    pub skill_persuasion: Option<Skill>,
-    pub skill_religion: Option<Skill>,
-    pub skill_sleight_of_hand: Option<Skill>,
-    pub skill_stealth: Option<Skill>,
-    pub skill_survival: Option<Skill>,
+    pub skill_acrobatics: Option<Proficiency>,
+    pub skill_animal_handling: Option<Proficiency>,
+    pub skill_athletics: Option<Proficiency>,
+    pub skill_deception: Option<Proficiency>,
+    pub skill_history: Option<Proficiency>,
+    pub skill_insight: Option<Proficiency>,
+    pub skill_inimidation: Option<Proficiency>,
+    pub skill_investigation: Option<Proficiency>,
+    pub skill_medicine: Option<Proficiency>,
+    pub skill_nature: Option<Proficiency>,
+    pub skill_perception: Option<Proficiency>,
+    pub skill_performance: Option<Proficiency>,
+    pub skill_persuasion: Option<Proficiency>,
+    pub skill_religion: Option<Proficiency>,
+    pub skill_sleight_of_hand: Option<Proficiency>,
+    pub skill_stealth: Option<Proficiency>,
+    pub skill_survival: Option<Proficiency>,
     pub passive_wisdom: Option<String>,
     pub armor_class: Option<String>,
     pub initiative: Option<String>,
@@ -97,8 +97,8 @@ impl From<Vec<CharacterDelta>> for CharacterData {
             _ => None,
         };
 
-        let get_skill = |field_name: &str| match &data.get(field_name) {
-            Some(FieldValue::Skill(value)) => Some(value.clone()),
+        let get_proficiency = |field_name: &str| match &data.get(field_name) {
+            Some(FieldValue::Proficiency(value)) => Some(value.clone()),
             _ => None,
         };
 
@@ -119,31 +119,31 @@ impl From<Vec<CharacterDelta>> for CharacterData {
             score_charisma: get_string("score_charisma"),
             inspiration: get_string("inspiration"),
             proficiency_bonus: get_string("proficiency_bonus"),
-            saving_throw_str: get_string("saving_throw_str"),
-            saving_throw_dexterity: get_string("saving_throw_dexterity"),
-            saving_throw_constitution: get_string("saving_throw_constitution"),
-            saving_throw_intelligence: get_string("saving_throw_intelligence"),
-            saving_throw_wisdom: get_string("saving_throw_wisdom"),
-            saving_throw_charisma: get_string("saving_throw_charisma"),
+            saving_throw_strength: get_proficiency("saving_throw_strength"),
+            saving_throw_dexterity: get_proficiency("saving_throw_dexterity"),
+            saving_throw_constitution: get_proficiency("saving_throw_constitution"),
+            saving_throw_intelligence: get_proficiency("saving_throw_intelligence"),
+            saving_throw_wisdom: get_proficiency("saving_throw_wisdom"),
+            saving_throw_charisma: get_proficiency("saving_throw_charisma"),
             death_save_fail: get_string("death_save_fail"),
             death_save_success: get_string("death_save_success"),
-            skill_acrobatics: get_skill("skill_acrobatics"),
-            skill_animal_handling: get_skill("skill_animal_handling"),
-            skill_athletics: get_skill("skill_athletics"),
-            skill_deception: get_skill("skill_deception"),
-            skill_history: get_skill("skill_history"),
-            skill_insight: get_skill("skill_insight"),
-            skill_inimidation: get_skill("skill_inimidation"),
-            skill_investigation: get_skill("skill_investigation"),
-            skill_medicine: get_skill("skill_medicine"),
-            skill_nature: get_skill("skill_nature"),
-            skill_perception: get_skill("skill_perception"),
-            skill_performance: get_skill("skill_performance"),
-            skill_persuasion: get_skill("skill_persuasion"),
-            skill_religion: get_skill("skill_religion"),
-            skill_sleight_of_hand: get_skill("skill_sleight_of_hand"),
-            skill_stealth: get_skill("skill_stealth"),
-            skill_survival: get_skill("skill_survival"),
+            skill_acrobatics: get_proficiency("skill_acrobatics"),
+            skill_animal_handling: get_proficiency("skill_animal_handling"),
+            skill_athletics: get_proficiency("skill_athletics"),
+            skill_deception: get_proficiency("skill_deception"),
+            skill_history: get_proficiency("skill_history"),
+            skill_insight: get_proficiency("skill_insight"),
+            skill_inimidation: get_proficiency("skill_inimidation"),
+            skill_investigation: get_proficiency("skill_investigation"),
+            skill_medicine: get_proficiency("skill_medicine"),
+            skill_nature: get_proficiency("skill_nature"),
+            skill_perception: get_proficiency("skill_perception"),
+            skill_performance: get_proficiency("skill_performance"),
+            skill_persuasion: get_proficiency("skill_persuasion"),
+            skill_religion: get_proficiency("skill_religion"),
+            skill_sleight_of_hand: get_proficiency("skill_sleight_of_hand"),
+            skill_stealth: get_proficiency("skill_stealth"),
+            skill_survival: get_proficiency("skill_survival"),
             passive_wisdom: get_string("passive_wisdom"),
             armor_class: get_string("armor_class"),
             initiative: get_string("initiative"),
@@ -174,7 +174,7 @@ impl From<CharacterData> for HashMap<String, FieldValue> {
         let mut data = HashMap::<String, FieldValue>::new();
 
         let fv_string = |v: Option<String>| FieldValue::String(v.unwrap_or_default());
-        let fv_skill = |v: Option<Skill>| FieldValue::Skill(v.unwrap_or_default());
+        let fv_proficiency = |v: Option<Proficiency>| FieldValue::Proficiency(v.unwrap_or_default());
 
         data.insert("class".to_string(), fv_string(value.class));
         data.insert("level".to_string(), fv_string(value.level));
@@ -211,28 +211,28 @@ impl From<CharacterData> for HashMap<String, FieldValue> {
             fv_string(value.proficiency_bonus),
         );
         data.insert(
-            "saving_throw_str".to_string(),
-            fv_string(value.saving_throw_str),
+            "saving_throw_strength".to_string(),
+            fv_proficiency(value.saving_throw_strength),
         );
         data.insert(
             "saving_throw_dexterity".to_string(),
-            fv_string(value.saving_throw_dexterity),
+            fv_proficiency(value.saving_throw_dexterity),
         );
         data.insert(
             "saving_throw_constitution".to_string(),
-            fv_string(value.saving_throw_constitution),
+            fv_proficiency(value.saving_throw_constitution),
         );
         data.insert(
             "saving_throw_intelligence".to_string(),
-            fv_string(value.saving_throw_intelligence),
+            fv_proficiency(value.saving_throw_intelligence),
         );
         data.insert(
             "saving_throw_wisdom".to_string(),
-            fv_string(value.saving_throw_wisdom),
+            fv_proficiency(value.saving_throw_wisdom),
         );
         data.insert(
             "saving_throw_charisma".to_string(),
-            fv_string(value.saving_throw_charisma),
+            fv_proficiency(value.saving_throw_charisma),
         );
         data.insert(
             "death_save_fail".to_string(),
@@ -244,51 +244,51 @@ impl From<CharacterData> for HashMap<String, FieldValue> {
         );
         data.insert(
             "skill_acrobatics".to_string(),
-            fv_skill(value.skill_acrobatics),
+            fv_proficiency(value.skill_acrobatics),
         );
         data.insert(
             "skill_animal_handling".to_string(),
-            fv_skill(value.skill_animal_handling),
+            fv_proficiency(value.skill_animal_handling),
         );
         data.insert(
             "skill_athletics".to_string(),
-            fv_skill(value.skill_athletics),
+            fv_proficiency(value.skill_athletics),
         );
         data.insert(
             "skill_deception".to_string(),
-            fv_skill(value.skill_deception),
+            fv_proficiency(value.skill_deception),
         );
-        data.insert("skill_history".to_string(), fv_skill(value.skill_history));
-        data.insert("skill_insight".to_string(), fv_skill(value.skill_insight));
+        data.insert("skill_history".to_string(), fv_proficiency(value.skill_history));
+        data.insert("skill_insight".to_string(), fv_proficiency(value.skill_insight));
         data.insert(
             "skill_inimidation".to_string(),
-            fv_skill(value.skill_inimidation),
+            fv_proficiency(value.skill_inimidation),
         );
         data.insert(
             "skill_investigation".to_string(),
-            fv_skill(value.skill_investigation),
+            fv_proficiency(value.skill_investigation),
         );
-        data.insert("skill_medicine".to_string(), fv_skill(value.skill_medicine));
-        data.insert("skill_nature".to_string(), fv_skill(value.skill_nature));
+        data.insert("skill_medicine".to_string(), fv_proficiency(value.skill_medicine));
+        data.insert("skill_nature".to_string(), fv_proficiency(value.skill_nature));
         data.insert(
             "skill_perception".to_string(),
-            fv_skill(value.skill_perception),
+            fv_proficiency(value.skill_perception),
         );
         data.insert(
             "skill_performance".to_string(),
-            fv_skill(value.skill_performance),
+            fv_proficiency(value.skill_performance),
         );
         data.insert(
             "skill_persuasion".to_string(),
-            fv_skill(value.skill_persuasion),
+            fv_proficiency(value.skill_persuasion),
         );
-        data.insert("skill_religion".to_string(), fv_skill(value.skill_religion));
+        data.insert("skill_religion".to_string(), fv_proficiency(value.skill_religion));
         data.insert(
             "skill_sleight_of_hand".to_string(),
-            fv_skill(value.skill_sleight_of_hand),
+            fv_proficiency(value.skill_sleight_of_hand),
         );
-        data.insert("skill_stealth".to_string(), fv_skill(value.skill_stealth));
-        data.insert("skill_survival".to_string(), fv_skill(value.skill_survival));
+        data.insert("skill_stealth".to_string(), fv_proficiency(value.skill_stealth));
+        data.insert("skill_survival".to_string(), fv_proficiency(value.skill_survival));
         data.insert(
             "passive_wisdom".to_string(),
             fv_string(value.passive_wisdom),
--- static/js/main.js
+++ static/js/main.js
@@ -8,19 +8,108 @@
     window.apiModel = apiModel;
     window.formModel = formModel;
 
-    const fieldsWrapper = document.querySelector(".fields-wrapper");
+    const fields = document.querySelector(".fields-wrapper");
 
-    const fields = Object.keys(formModel).map(createFormField);
+    fields.appendChild(
+        createFieldGroup(
+            "saving throws",
+            createProficiencyField,
+            [
+                ["saving_throw_strength", "Strength"],
+                ["saving_throw_dexterity", "Dexterity"],
+                ["saving_throw_constitution", "Constitution"],
+                ["saving_throw_intelligence", "Intelligence"],
+                ["saving_throw_wisdom", "Wisdom"],
+                ["saving_throw_charisma", "Charisma"],
+            ]
+        )
+    )
 
-    const groupSkills = createFieldGroup("skills", [
-        ""
-    ])
+    fields.appendChild(
+        createFieldGroup(
+            "skills",
+            createProficiencyField,
+            [
+                ["skill_acrobatics", "Acrobatics"],
+                ["skill_animal_handling", "Animal Handling"],
+                ["skill_athletics", "Athletics"],
+                ["skill_deception", "Deception"],
+                ["skill_history", "History"],
+                ["skill_insight", "Insight"],
+                ["skill_inimidation", "Intimidation"],
+                ["skill_investigation", "Investigation"],
+                ["skill_medicine", "Medicine"],
+                ["skill_nature", "Nature"],
+                ["skill_perception", "Perception"],
+                ["skill_performance", "Performance"],
+                ["skill_persuasion", "Persuasion"],
+                ["skill_religion", "Religion"],
+                ["skill_sleight_of_hand", "Sleight of Hand"],
+                ["skill_stealth", "Stealth"],
+                ["skill_survival", "Survival"],
+            ]
+        )
+    );
 
-    function createFieldGroup(groupName, fieldNames) {
+    function createFieldGroup(groupName, fieldFactory, fieldNames) {
+        const group = document.createElement("section");
 
+        const groupHeader = document.createElement("h3");
+        groupHeader.textContent = groupName;
+
+        group.appendChild(groupHeader);
+
+        fieldNames
+            .map((fieldName) => {
+                if (typeof fieldName === "string") {
+                    return fieldFactory(fieldName, fieldName);
+                } else {
+                    return fieldFactory(fieldName[0], fieldName[1]);
+                }
+            })
+            .forEach(field => {
+                group.appendChild(field)
+            });
+
+        return group;
     }
 
-    function createFormField(fieldName) {
+
+
+    function createProficiencyField(fieldName, label) {
+        const wrapper = document.createElement("div");
+
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.title = "Check the box to indicate proficiency";
+        checkbox.id = `${fieldName}-proficient`;
+        checkbox.checked = formModel[fieldName].proficient;
+
+        const modifier = document.createElement("input");
+        modifier.id = `${fieldName}-modifier`;
+        modifier.placeholder = "modifier";
+        modifier.value = formModel[fieldName].modifier;
+        
+        const fieldLabel = document.createElement("label");
+        fieldLabel.setAttribute("for", `${fieldName}-modifier`);
+        fieldLabel.textContent = label;
+
+        checkbox.addEventListener("input", (ev) => {
+            formModel[fieldName].proficient = ev.target.checked;
+        });
+
+        modifier.addEventListener("input", (ev) => {
+            formModel[fieldName].modifier = ev.target.value;
+        });
+
+        wrapper.appendChild(checkbox);
+        wrapper.appendChild(fieldLabel);
+        wrapper.appendChild(modifier);
+
+        return wrapper
+    }
+
+    function createTextField(fieldName) {
         const elem = document.createElement("div");
 
         const label = document.createElement("label");
